914b2681ee90ac1e31068075e9daa7cccdb43459
mekamdan <m.alencar.v@outlook.com>
2019-11-25 04:07:57 -0300

    make it better

--- onlibrary/models.py
+++ onlibrary/models.py
@@ -11,13 +11,13 @@ def accept():
 
 # Create your models here.
 class Linguagem(models.Model):
-    lingua = models.CharField('Linguagem',max_length=30, default="Português-Br")
+    lingua = models.CharField('Lingua',max_length=30, default="Português",unique=True)
     
     def __str__(self):
         return self.lingua
 
 class Jornal(models.Model):
-    nome = models.CharField('Nome do Jornal', max_length=50)
+    nome = models.CharField('Nome do Jornal', max_length=50,unique=True)
     email= models.CharField('e-Mail do Jornal', max_length=100)
     site=models.CharField('site do Jornal', max_length=100)
 
@@ -25,31 +25,31 @@ class Jornal(models.Model):
         return self.jornal
 
 class Categoria(models.Model):
-    categoria= models.CharField("Categoria",max_length=50)
+    categoria= models.CharField("Categoria",max_length=50,unique=True)
 
     def __str__(self):
         return self.categoria
 
 
 class Tema(models.Model):
-    tema=models.CharField("Tema",max_length=60)
+    tema=models.CharField("Tema",max_length=60,unique=True)
     def __str__(self):
         return self.tema
 
 class Envolvidos(models.Model):
-    nome=models.CharField("Nome",max_length=200)
+    nome=models.CharField("Nome",max_length=200,unique=True)
     def __str__(self):
         return self.nome
 
 class Tipo_midia(models.Model):
-    tipo = models.CharField('Título',max_length=300,help_text="Nome da produção")
+    tipo = models.CharField('Tipo de Mídia',max_length=254,help_text="Nome da produção", unique=True)
 
 class Formato(models.Model):
-    formato=models.CharField("Formato",max_length=6)
+    formato=models.CharField("Formato",max_length=6,unique=True)
     midia = models.ForeignKey(Tipo_midia, on_delete=models.PROTECT)
 
 class Producao(models.Model):
-    titulo=models.CharField('Título',max_length=300,help_text="Nome da produção")
+    titulo=models.CharField('Título',max_length=254,help_text="Nome da produção",unique=True)
     descricao=models.TextField('Descrição', blank=True)
     data = models.DateField('Data do Documento', default=django.utils.timezone.now, help_text="Use formato dd/mm/AAAA")
     accepted = models.BooleanField('Accept file', choices=accept(), default=False)
